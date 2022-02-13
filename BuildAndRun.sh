2400d3095f9e03001492ee9a2e4855244503c0fb
Roger Johnson <rkj8@students.uwf.edu>
2022-02-13 01:14:10 +0000

    Update build and run executable

--- BuildAndRun.sh
+++ BuildAndRun.sh
@@ -1,12 +1,21 @@
+#Prepare environment
+mkdir src/build/
+mkdir src/build/classes
+mkdir src/package
+cp src/*.java src/package/
+
+# Create class intermediates
 javac -sourcepath src -d src/build/classes src/package/*.java
-cd src/package/
-cp *.java ../
-cd ..
+
 #java -classpath build/classes/ Program
-echo Main-Class: Program>myManifest
-jar cfm build/ConnectM.jar myManifest -C build/classes/ .
-cp build/ConnectM.jar ../ConnectM.jar
-rm build/classes/*
-rm myManifest
-rm *.java
+echo Main-Class: Program>src/myManifest
+jar cfm src/build/ConnectM.jar src/myManifest -C src/build/classes/ src/
+cp src/build/ConnectM.jar ConnectM.jar
+
+# Clean up intermediates
+rm -r src/build
+rm src/myManifest
+rm -r src/package
+
+# Start program
 java -jar build/ConnectM.jar
