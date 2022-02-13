1c456abd14ab63a714f5f8351e3ff1ce9d0f7721
Roger Johnson <rkj8@students.uwf.edu>
2022-02-13 01:26:15 +0000

    Updates to build script and readme

--- BuildAndRun.sh
+++ BuildAndRun.sh
@@ -1,21 +1,30 @@
 #Prepare environment
+echo "Preparing environment"
 mkdir src/build/
 mkdir src/build/classes
 mkdir src/package
 cp src/*.java src/package/
 
 # Create class intermediates
+echo "Creating class intermediates"
 javac -sourcepath src -d src/build/classes src/package/*.java
 
 #java -classpath build/classes/ Program
+ls
+echo "Creating manifest"
 echo Main-Class: Program>src/myManifest
+
+echo "Creating jar"
 jar cfm src/build/ConnectM.jar src/myManifest -C src/build/classes/ src/
 cp src/build/ConnectM.jar ConnectM.jar
 
 # Clean up intermediates
+echo "Cleaning Up"
 rm -r src/build
 rm src/myManifest
 rm -r src/package
 
 # Start program
-java -jar build/ConnectM.jar
+echo "Starting program"
+java -jar ConnectM.jar
+
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # CAPConnectM
 # Roger Johnson & Justin Odel
 
+Building and Running:
+    - Ensure that user has executable permissions for BuildAndRun.sh
+        chmod u+x BuildAndRun.sh
+
+    - An executable jar will be created in the parent directory of the repository
+
+    - This executable can be ran:
+        java -jar ConnectM.jar
