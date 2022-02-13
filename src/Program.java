67bc5191046f38ee207ba55aed2277ec8661c8aa
Roger Johnson <rkj8@students.uwf.edu>
2022-02-13 00:27:41 +0000

    Some setup

---
+++ src/Program.java
@@ -0,0 +1,23 @@
+package capconnectm;
+
+public class Program {
+    public static void main(String[] args) { 
+        
+        // TODO : Set up Gameboard based on command
+        //         line arguments
+
+
+        // TODO : Create message publisher
+        //         in order to send moves
+
+        // TODO : Create player objects (Human vs. Computer)
+        //        The player objects will send actions to the 
+        //        board. The board will wait for commands and 
+        //        print the layout and request input from user
+        //        if required
+
+
+        // Print hello world
+        System.out.println("Hello, World");
+    }
+}
