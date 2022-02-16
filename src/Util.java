1cf34684f889df4a20f1e6b3b414f7e5d207d8f6
Roger Johnson <rkj8@students.uwf.edu>
2022-02-16 04:24:56 +0000

    fix

--- src/Util.java
+++ src/Util.java
@@ -1,5 +1,5 @@
 public class Util {
-    public static int[] getAvailableMoves(GameBoard currentBoard, IPlayer player) {
+    public static int[] getAvailableMoves(Gameboard currentBoard, IPlayer player) {
         // TODO : Look at the board and return an array of possible moves.
         //        This will end up being used by
     }
